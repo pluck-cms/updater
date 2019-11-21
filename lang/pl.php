973a81216f32bf05c38066562a95554183cfd5d6
ekyo-pl <ekyo-pl@users.noreply.github.com>
2019-11-21 12:38:33 +0100

    pl translation file
    pl translation file
    one mistake in en lang file fix.

---
+++ lang/pl.php
@@ -0,0 +1,24 @@
+<?php
+//lang file for updater module
+//part of Pluck-cms
+//Translation Bogumił "ekyo" Cieniek
+
+$lang['updater']['name'] = 'Pluck - Aktualizacja na miejscu';
+$lang['updater']['intro'] = 'Ten moduł pozwala na aktualizacje Plucka do najnowszej wersji.';
+$lang['updater']['author'] = 'Bas Steelooper';
+$lang['updater']['main'] = 'Wybierz, aby sprawdzić wymagania, dostępność, lub wykonać aktualizację.';
+$lang['updater']['requirements'] = 'Sprawdź wymagania';
+$lang['updater']['available'] = 'Sprawdź dostępne aktualizacje';
+$lang['updater']['backup'] = 'Utwórz kopię zapasową witryny';
+$lang['updater']['update'] = 'Zaktualizuj do najnowszej wersji';
+$lang['updater']['requirements_info'] = 'Sprawdzi instalację, czy aktualizacja może być uruchomiona.';
+$lang['updater']['available_info'] = 'Sprawdzi, czy aktualizacja jest dostępna';
+$lang['updater']['backup_info'] = 'Utwórz kopię zapasową strony internetowej w folderze plików';
+$lang['updater']['update_info'] = 'Zaktualizuje Plucka do najnowszej wersji';
+$lang['updater']['yes'] = "Istnieje nowsza wersja Plucka";
+$lang['updater']['urgent'] = "Istnieje wersja bezpieczeństwa Plucka";
+$lang['updater']['no'] = "Najnowsza wersja Pluck jest obecnie zainstalowana";
+$lang['updater']['curl_installed'] = "Curl jest dostępny do pobierania";
+$lang['updater']['curl_notinstalled'] = "Curl nie jest dostępny, narzędzie aktualizacji nie może kontynuować";
+$lang['updater']['backupcreated'] = "Kopia zapasowa została utworzona";
+$lang['updater']['not_needed'] = "Aktualizacja nie jest potrzebna.";
