df30a8ef4fe0755c89dcc7bfd5cf1cdc3f90ccdf
Bas Steelooper <BSteelooper@users.noreply.github.com>
2025-03-23 10:59:50 +0100

    Duplicate function

--- updater.admin.php
+++ updater.admin.php
@@ -246,18 +246,6 @@ function checkUpdate(){
 		return $lastPart;
 
 }
-   // Function to remove folders and files 
-   function rrmdir($dir) {
-	if (is_dir($dir)) {
-		$files = scandir($dir);
-		foreach ($files as $file)
-			if ($file != "." && $file != "..") rrmdir("$dir/$file");
-		rmdir($dir);
-	}
-	else if (file_exists($dir)) unlink($dir);
-}
-
-
 
    // Function to remove folders and files 
    function rrmdir($dir) {
